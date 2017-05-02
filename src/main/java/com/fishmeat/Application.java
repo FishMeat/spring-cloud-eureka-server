66a216923ad5d965509c2827d9245ac0e3169779
sggb <shangggb@joyowo.com>
2017-05-02 14:29:59 +0800

    增加eureka配置

---
+++ src/main/java/com/fishmeat/Application.java
@@ -0,0 +1,17 @@
+package com.fishmeat;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author sggb
+ * @since 02.05.2017
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+}
